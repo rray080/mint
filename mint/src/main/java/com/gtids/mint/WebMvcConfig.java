b934a8a44441356416f56568bf2b924f0eadcbab
pirateshappy <sandeepprasad5678@gmail.com>
2019-07-29 02:41:10 +0530

    database change.authantication controller added,login register home page add

---
+++ mint/src/main/java/com/gtids/mint/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.gtids.mint;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public abstract class WebMvcConfig implements WebMvcConfigurer{
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+}
+
+
